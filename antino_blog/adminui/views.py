3080f52131165bc8cff93ee0115f7652861dd31a
Priyanshu Katiyar <ktrzorion@gmail.com>
2024-04-09 17:46:20 +0530

    Removed Vue Frontend and Integrated Summary Charts with Backend

--- antino_blog/adminui/views.py
+++ antino_blog/adminui/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import FileResponse
 
-# Create your views here.
+def weekly_report(request):
+    file_path = '/home/ktr_zorion/Downloads/Antino Blogging Proj/antino_blog/adminui/templates/admin/weekly_summary.html'
+    return FileResponse(open(file_path, 'rb'), content_type='text/html')
+
+def monthly_report(request):
+    file_path = '/home/ktr_zorion/Downloads/Antino Blogging Proj/antino_blog/adminui/templates/admin/monthly_summary.html'
+    return FileResponse(open(file_path, 'rb'), content_type='text/html')
+
+def quaterly_report(request):
+    file_path = '/home/ktr_zorion/Downloads/Antino Blogging Proj/antino_blog/adminui/templates/admin/quaterly_summary.html'
+    return FileResponse(open(file_path, 'rb'), content_type='text/html')
+
+def yearly_report(request):
+    file_path = '/home/ktr_zorion/Downloads/Antino Blogging Proj/antino_blog/adminui/templates/admin/yearly_summary.html'
+    return FileResponse(open(file_path, 'rb'), content_type='text/html')
--- antino_blog/antino_blog/urls.py
+++ antino_blog/antino_blog/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from adminui import views
 
 admin.site.site_header = "-- Antino Blogging --"
 admin.site.site_title = "Welcome to Antino Blogging"
@@ -9,4 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
     path('api/', include('authapp.urls')),
+    path('weekly-report/', views.weekly_report, name='weekly_report'),
+    path('monthly-report/', views.monthly_report, name='monthly_report'),
+    path('quaterly-report/', views.quaterly_report, name='quaterly_report'),
+    path('yearly-report/', views.yearly_report, name='yearly_report'),
 ]
